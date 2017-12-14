8259dbb43292c4a2e1fc20bdaadeb0a75386a8f6
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2017-12-13 23:54:53 -0800

    parse.hs requires leading space

--- 2_parsing/parse.hs
+++ 2_parsing/parse.hs
@@ -1,12 +1,15 @@
-import Text.ParserCombinators.Parsec hiding (space)
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 main :: IO ()
 main = do
