6953c3e36f0563943995311accfa9dbf8a42f4fd
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2017-12-14 00:03:11 -0800

    parse.hs add LispVal type, parseString, parseAtom

--- 2_parsing/parse.hs
+++ 2_parsing/parse.hs
@@ -11,6 +11,30 @@ readExpr input = case parse (spaces >> symbol) "lisp" input of
 spaces :: Parser ()
 spaces = skipMany1 space
 
+data LispVal = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LipVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
 main :: IO ()
 main = do
   (expr:_) <- getArgs
